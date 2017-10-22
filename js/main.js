b4e2ff7860e512bd834fd323e67932d7f06a2830
Loukas Anastasiou <loukas_anastasiou@outlook.com>
2017-10-22 09:36:35 +0200

    First Commit
    A few changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 jQuery(function ($) {
+    'use strict';
     $('.search').keyup(function () {
-        var filter = $(this).val(), count = 0;
+        var filter = $(this).val();
         $('a[data-title]').each(function (index, elem) {
             if ($(elem).attr('data-title').search(filter) < 0) {
                 $(elem).addClass('hidden');
             } else {
                 $(elem).removeClass('hidden');
-                count++;
             }
         });
     });
