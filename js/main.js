9b2e9cadb73e418210dc1a3272ac7fc26a6e1c99
Loukas Anastasiou <loukas_anastasiou@outlook.com>
2017-10-22 09:39:51 +0200

    Seccond Commit
    Case insensitivity

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(function ($) {
     $('.search').keyup(function () {
         var filter = $(this).val();
         $('a[data-title]').each(function (index, elem) {
-            if ($(elem).attr('data-title').search(filter) < 0) {
+            if ($(elem).attr('data-title').toLowerCase().search(filter.toLowerCase()) < 0) {
                 $(elem).addClass('hidden');
             } else {
                 $(elem).removeClass('hidden');
